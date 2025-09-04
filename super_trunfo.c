e2bbc208b0e3ce35908ebfde08b8fff7dbf97e27
Douglas <dugbits@gmail.com>
2025-09-04 03:43:32 +0000

    Fase 2 da lógica do Super Trunfo em C: Menu de atributos para comparação

--- super_trunfo.c
+++ super_trunfo.c
@@ -2,57 +2,16 @@
 #include <string.h> // Pois o console do Codespaces estava acusando um problema na linha 25 e 45, onde resolvi o problema do \n inserido pelo fgets.
 int main(){
     // Declaração de variáveis:
-    char estado1[20], estado2[20];
-    char codigo1[4], codigo2[4];
-    char cidade1[30], cidade2[30];
-    int populacao1, populacao2;
-    float areakm1, areakm2;
-    float pib1, pib2, pibCap1, pibCap2, denPop1, denPop2;
-    int tour1, tour2;
+    char estado1[20]="A", estado2[20]="B";
+    char codigo1[4]="A01", codigo2[4]="B02";
+    char cidade1[30]="São Paulo", cidade2[30]="Rio de Janeiro";
+    int populacao1=12325000, populacao2=6748000;
+    float areakm1=1521.11, areakm2=1200.25;
+    float pib1=699.28, pib2=300.50, pibCap1, pibCap2, denPop1, denPop2;
+    int tour1=50, tour2=30;
     int resPop, resTour, resArea, resPib, resPibCap, resDenPop, resSuper;
     double super1, super2;
-
-    // Entrada dos dados (Carta 1):
-    printf("====SUPER  TRUNFO====\n");
-    printf("| CADASTRO DA CARTA 1 |\n");
-    printf("Digite o NOME DO ESTADO: ");
-    fgets(estado1, 20, stdin);
-    estado1[strcspn(estado1, "\n")] = 0; // Remove o '\n' que o fgets recebe da entrada.
-    printf("Digite o CÓDIGO DA CARTA: ");
-    scanf("%s", codigo1);
-    getchar(); // Limpa o buffer do teclado e evita que o fgets seja pulado.
-    printf("Digite o NOME DA CIDADE: ");
-    fgets(cidade1, 30, stdin);
-    cidade1[strcspn(cidade1, "\n")] = 0; // Remove o '\n' que o fgets recebe da entrada.
-    printf("Digite a POPULAÇÃO: ");
-    scanf("%d", &populacao1);
-    printf("Digite a ÁREA EM KM²: ");
-    scanf("%f", &areakm1);
-    printf("Digite o PIB (Produto Interno Bruto) da cidade: ");
-    scanf("%f", &pib1);
-    printf("Digite quantos PONTOS TURÍSTICOS a cidade tem: ");
-    scanf("%d", &tour1);
-    getchar(); // Limpa o buffer do teclado novamente antes de ler a próxima entrada.
-
-    // Entrada dos dados (Carta 2):
-    printf("\n| CADASTRO DA CARTA 2 |\n");
-    printf("Digite o NOME DO ESTADO: ");
-    fgets(estado2, 20, stdin);
-    estado2[strcspn(estado2, "\n")] = 0;
-    printf("Digite o CÓDIGO DA CARTA: ");
-    scanf("%s", codigo2);
-    getchar();
-    printf("Digite o NOME DA CIDADE: ");
-    fgets(cidade2, 30, stdin);
-    cidade2[strcspn(cidade2, "\n")] = 0;
-    printf("Digite a POPULAÇÃO: ");
-    scanf("%d", &populacao2);
-    printf("Digite a ÁREA EM KM²: ");
-    scanf("%f", &areakm2);
-    printf("Digite o PIB (Produto Interno Bruto) da cidade: ");
-    scanf("%f", &pib2);
-    printf("Digite quantos PONTOS TURÍSTICOS a cidade tem: ");
-    scanf("%d", &tour2);
+    int opcao;
 
     // Processamento dos dados:
     denPop1 = populacao1 / areakm1;
@@ -87,16 +46,101 @@ int main(){
     printf("DENSIDADE POPULACIONAL- %.2f hab/km²\n", denPop2);
     printf("PIB PER CAPITA- %.2f reais\n", pibCap2);
     printf("SUPER PODER- %.3f\n\n", super2);
+    printf("Qual atributo vamos comparar?\n");
+    printf("1. POPULAÇÃO\n");
+    printf("2. ÁREA EM km²\n");
+    printf("3. PIB\n");
+    printf("4. PONTOS TURÍSTICOS\n");
+    printf("5. DENSIDADE POPULACIONAL\n");
+    printf("6. PIB PER CAPITA\n");
+    printf("7. SUPER PODER\n");
+    scanf("%d", &opcao);
+    printf("==HORA DA BATALHA==\n");
+    printf("%s VS %s\n", cidade1, cidade2);
+    printf("ATRIBUTO ESCOLHIDO:\n");
 
-    // Estrutura condicional para comparação de um atributo de duas cartas
-    printf("=====COMPARAÇÃO DAS CARTAS======\n");
-    printf("ATRIBUTO: DENSIDADE POPULACIONAL\n");
-    printf("Carta 1 - %s (%s): %.2f hab/km²\n", cidade1, estado1, denPop1);
-    printf("Carta 2 - %s (%s): %.2f hab/km²\n", cidade2, estado2, denPop2);
-    if (denPop1 < denPop2) {
-        printf("Resultado: Carta 1 (%s) venceu!!\n", cidade1);
-    } else {
-        printf("Resultado: Carta 2 (%s) venceu!!\n", cidade2);
+    // Uso do switch para formar o menu e separar as comparações de cada atributo
+    switch(opcao){
+        case 1:
+            printf("=====POPULAÇÃO=====\n");
+            printf("%s: %d\n", cidade1, populacao1);
+            printf("%s: %d\n", cidade2, populacao2);
+            if (populacao1 > populacao2){
+                printf("%s venceu!", cidade1);
+            } else if (populacao1 < populacao2) {
+                printf("%s venceu!", cidade2);
+            } else {
+                printf("EMPATE!");
+            } break;
+        case 2:
+            printf("====ÁREA EM km²====\n");
+            printf("%s: %.2f\n", cidade1, areakm1);
+            printf("%s: %.2f\n", cidade2, areakm2);
+            if (areakm1 > areakm2){
+                printf("%s venceu!", cidade1);
+            } else if(areakm1 < areakm2){
+                printf("%s venceu!", cidade2);
+            } else {
+                printf("EMPATE!");
+            } break;
+        case 3:
+            printf("========PIB========\n");
+            printf("%s: %.2f\n", cidade1, pib1);
+            printf("%s: %.2f\n", cidade2, pib2);
+            if (pib1 > pib2){
+                printf("%s venceu!", cidade1);
+            } else if (pib1 < pib2){
+                printf("%s venceu!", cidade2);
+            } else {
+                printf("EMPATE!");
+            } break;
+        case 4:
+            printf("=PONTOS TURÍSTICOS=\n");
+            printf("%s: %d\n", cidade1, tour1);
+            printf("%s: %d\n", cidade2, tour2);
+            if (tour1 > tour2){
+                printf("%s venceu!", cidade1);
+            } else if (tour1 < tour2){
+                printf("%s venceu!", cidade2);
+            } else {
+                printf("EMPATE!");
+            } break;
+        case 5:
+            printf("DENSIDADE POPULACIONAL\n");
+            printf("%s: %.2f\n", cidade1, denPop1);
+            printf("%s: %.2f\n", cidade2, denPop2);
+            if (denPop1 < denPop2){
+                printf("%s venceu!", cidade1);
+            } else if (denPop1 > denPop2){
+                printf("%s venceu!", cidade2);
+            } else {
+                printf("EMPATE!");
+            } break;
+        case 6:
+            printf("===PIB PER CAPITA==\n");
+            printf("%s: %.2f\n", cidade1, pibCap1);
+            printf("%s: %.2f\n", cidade2, pibCap2);
+            if (pibCap1 > pibCap2){
+                printf("%s venceu!", cidade1);
+            } else if (pibCap1 < pibCap2){
+                printf("%s venceu!", cidade2);
+            } else {
+                printf("EMPATE!");
+            } break;
+        case 7:
+            printf("====SUPER PODER====\n");
+            printf("%s: %.2f\n", cidade1, super1);
+            printf("%s: %.2f\n", cidade2, super2);
+            if (super1 > super2){
+                printf("%s venceu!", cidade1);
+            } else if (super1 < super2){
+                printf("%s venceu!", cidade2);
+            } else {
+                printf("EMPATE!");
+            } break;
+            default:
+            printf("Entrada inválida");
     }
+
     return 0;
 }
